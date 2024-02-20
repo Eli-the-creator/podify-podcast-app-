13b45add72f6ec207025e79159e2d019edf644e5
ELI-THE_CREATOR <neverlookbackeli@gmail.com>
2024-02-20 15:02:39 +0000

    last commit, add the delete audio from playlist

---
+++ src/components/profile/PublicPlaylistTab.tsx
@@ -0,0 +1,52 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import PlaylistItem from '@ui/PlaylistItem';
+import {FC} from 'react';
+import {View, StyleSheet, ScrollView} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {Playlist} from 'src/@types/audio';
+import {PublicProfileTabParamsList} from 'src/@types/navigation';
+import {useFetchPublicPlaylist} from 'src/hooks/query';
+import {
+  udpateAllowPlaylistAudioRemove,
+  updateIsPlaylistPrivate,
+  updatePlaylistVisbility,
+  updateSelectedListId,
+} from 'src/store/playlistModal';
+
+type Props = NativeStackScreenProps<
+  PublicProfileTabParamsList,
+  'PublicPlaylist'
+>;
+
+const PublicPlaylistTab: FC<Props> = props => {
+  const {data} = useFetchPublicPlaylist(props.route.params.profileId);
+
+  const dispatch = useDispatch();
+
+  const handleOnListPress = (playlist: Playlist) => {
+    dispatch(udpateAllowPlaylistAudioRemove(false));
+    dispatch(updateSelectedListId(playlist.id));
+    dispatch(updatePlaylistVisbility(true));
+    dispatch(updateIsPlaylistPrivate(playlist.visibility === 'private'));
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      {data?.map(playlist => {
+        return (
+          <PlaylistItem
+            onPress={() => handleOnListPress(playlist)}
+            key={playlist.id}
+            playlist={playlist}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default PublicPlaylistTab;
